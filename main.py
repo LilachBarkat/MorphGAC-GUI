b0b650cfea96248b78a02359eda9e052c2c6f63f
LilachBarkat <67235383+LilachBarkat@users.noreply.github.com>
2022-05-27 12:51:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,22 +3,26 @@
 
 """"
 ------------------------------------
-  Semi-automated segmentation GUI
+  MorphGAC segmentation GUI
 ------------------------------------
-This work provide a GUI for a Semi-automated segmentation.
-as part of the
-the code is based on the MorphGAC algorithm [2].
+This work provides a GUI for a Semi-automated segmentation.
+By a mouse click position, a number of iterations and a preprocessing image parameters,
+the MorphGAC algorithm [1] searches for the optimal object contour.
 
-References:
-.. [1] A Morphological Approach to Curvature-based Evolution of Curves and
+The GUI implementation enables to perform a Semi-automated segmentation in a simple manner,
+created as part of this research [2] for ultrasound breast tumors segmentation.
+The algorithm and its theoretical derivation are described in [1].
+
+If you use this implementation please cite the following papers:
+
+   [1] A Morphological Approach to Curvature-based Evolution of Curves and
        Surfaces, Pablo Márquez-Neila, Luis Baumela and Luis Álvarez. In IEEE
        Transactions on Pattern Analysis and Machine Intelligence (PAMI),
        2014, DOI 10.1109/TPAMI.2013.106
 
-.. [2] A Morphological Approach to Curvature-based Evolution of Curves and
-       Surfaces, Pablo Márquez-Neila, Luis Baumela and Luis Álvarez. In IEEE
-       Transactions on Pattern Analysis and Machine Intelligence (PAMI),
-       2014, DOI 10.1109/TPAMI.2013.106
+   [2] Barkat L, Freiman M, Azhari H. Image translation of Ultrasound to Pseudo
+       Anatomical Display by CycleGAN. arXiv preprint arXiv:2202.08053. 2022 Feb 16.
+
 """
 
 
@@ -29,7 +33,6 @@ import pandas as pd
 import matplotlib
 from matplotlib.widgets import TextBox, Button
 from matplotlib import pyplot as plt
-import threading
 from skimage.segmentation import (morphological_geodesic_active_contour,
                                   inverse_gaussian_gradient,
                                   disk_level_set)
@@ -216,7 +219,7 @@ class GUI:
         # Read image, apply Inverse Gaussian filter and initialize empty mask
         original_img = plt.imread(self.img_path)
         img = cv.imread(self.img_path)[..., 0] / 255.0
-        self.igg_img = inverse_gaussian_gradient(img, alpha=100, sigma=1.48)
+        self.igg_img = inverse_gaussian_gradient(img, alpha=100, sigma=1.5)
         self.mask = np.zeros_like(img)
 
         # Display the three images
